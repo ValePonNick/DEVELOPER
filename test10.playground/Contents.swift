d8e14833c6c2f834dc0668308d1b7a2fec9879af
valery ponomareva <valeryponomareva378he@gmail.com>
2025-01-08 23:27:25 +0700

    Developer
     создай два массива, объедини в один и выведи в консоль

---
+++ test10.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+// создай два массива оценок: 0 ... 5, 6 ... 10
+let arrayFirst = [0,1,2,3,4,5]
+let arraySecond = [6,7,8,9,10]
+// объедини два массива в один
+let sumArrays = arrayFirst + arraySecond
+print(sumArrays)

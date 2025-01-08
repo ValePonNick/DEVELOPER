1068a956855bb43e490bf3e111cf651f6241631b
valery ponomareva <valeryponomareva378he@gmail.com>
2025-01-08 14:58:04 +0700

    Developer
    добавила коммент

--- test6.playground/Contents.swift
+++ test6.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
 
-let array = ["Oin", "Gloin", "Balin", "Dwalin", "Nory", "Ory", "Fili", "Kili", "Thorin", "Bifur", "Bofur", "Bombur"]
-// создала массив с типом данных - строка
+var numberPi = 3.14
+// добавим комментарий
 

e3b0bd07ebbc5bd3b5fda7f0112e84f988af8dbc
valery ponomareva <valeryponomareva378he@gmail.com>
2025-01-08 13:23:48 +0700

    test5
    создала массив данных с типом строка

--- test5.playground/Contents.swift
+++ test5.playground/Contents.swift
@@ -3,3 +3,4 @@ import UIKit
 var nameCharacters = ["Bilbo", "Sam", "Pin", "Merry"]
 print(nameCharacters)
 // создала массив данных с типом строка
+// теперь добавь в уже существующий репозиторий Developer
